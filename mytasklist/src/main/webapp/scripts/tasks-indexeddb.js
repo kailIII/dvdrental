e3dbf14eb8acf987891260cf6945c26db2206ab5
Sanjay Singh <sanjays30@yahoo.com>
2014-09-03 09:54:08 +0530

    Jquery Example

---
+++ mytasklist/src/main/webapp/scripts/tasks-indexeddb.js
@@ -0,0 +1,78 @@
+storageEngine = function() {
+
+	var database; 
+	var objectStores;
+
+	 
+	
+	function getStorageObject(type){
+	}
+	return {
+		init : function(successCallback, errorCallback) {
+	
+			if (window.indexedDB) { 
+				var request = indexedDB.open(window.location.hostname +'DB', 1) ;
+				request.onsuccess = function(event) { 
+					database = request.result; 
+					successCallback( null); 
+					}; 
+				request.onerror = function( event) {
+				errorCallback(' storage_not_initalized', 'It is not possible to initialized storage'); 
+				} ;
+			} else { 
+					errorCallback(' storage_api_not_supported', 'The web storage api is not supported'); 
+			}
+
+			
+				
+		},
+		initObjectStore : function(type, successCallback, errorCallback) {
+			if (!database) { 
+				errorCallback( 'storage_api_not_initialized', 'The storage engine has not been initialized'); 
+			} 
+			var exists = false; 
+			$. each(database.objectStoreNames, function( i, v) { 
+				if (v == type) { 
+					exists = true;
+				}
+			});
+			
+			if (exists) { 
+				successCallback( null); 
+			} else { 
+					var version = database.version + 1; 
+					database.close(); 
+					var request = indexedDB.open(window.location.hostname +' DB', version); 
+					request.onsuccess = function(event) { 
+						successCallback(null); 
+					};
+					request.onerror = function(event) { 
+						errorCallback( 'storage_not_initalized', 'It is not possible to initialized storage'); 
+					};
+					request.onupgradeneeded = function(event) { 
+						database = event.target.result; 
+						var objectStore = database.createObjectStore(type, { keyPath: "id", autoIncrement: true });
+					};
+					
+			}
+			
+			
+			
+		},
+
+		save : function(type, obj, successCallback, errorCallback) {
+
+		},
+		findAll : function(type, successCallback, errorCallback) {
+	
+		},
+		deleteById : function(type, id, successCallback, errorCallback) {
+		},
+		findByProperty : function(type, propertyName, propertyValue,successCallback, errorCallback) {
+			
+		},
+		findById : function(type, id, successCallback, errorCallback) {
+
+		}
+	};
+}();
